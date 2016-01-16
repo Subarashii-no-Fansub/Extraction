4c994a2ce5238ceba5ded365d77ce9fcfe3e5745
Subarashii no Fansub <Subarashii-no-Fansub@users.noreply.github.com>
2016-01-16 17:07:21 +0100

    desactivate chmod

--- soustitre.sh
+++ soustitre.sh
@@ -2,13 +2,13 @@
 # paramètre 1 : video $1
 set -e
 
-chmod 644 "$1"
+#chmod 644 "$1"
 
 mkvmerge -i "$1"
 
 read -p "Numéro des sous-titres : " var_idss
 mkvextract tracks "$1" $var_idss:soustitre.ass
-chmod 644 "soustitre.ass"
+#chmod 644 "soustitre.ass"
 fullfilename=$(basename "$1")
 mv "soustitre.ass" "${fullfilename%.*}.ass"
 
