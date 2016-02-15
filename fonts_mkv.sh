ec37e0ce41aaa73575c909aca09d4ec7e971685a
Subarashii no Fansub <Subarashii-no-Fansub@users.noreply.github.com>
2016-02-15 01:02:25 +0100

    Fix the path

--- fonts_mkv.sh
+++ fonts_mkv.sh
@@ -8,7 +8,7 @@ fullfilename=$(basename "$1")
 mkdir -p "${fullfilename%.*}"
 cd "${fullfilename%.*}"
 
-attach=$(mkvmerge -i "$1" | grep Attachment | awk -F" " '{print $3}' | sed 's/://');
-mkvextract attachments "$1" $attach;
+attach=$(mkvmerge -i "../$1" | grep Attachment | awk -F" " '{print $3}' | sed 's/://');
+mkvextract attachments "../$1" $attach;
 
 echo "Fini !"
